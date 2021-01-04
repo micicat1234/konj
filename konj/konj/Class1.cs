8a610ceff8ff80b14d8f443dbd6bf208db6f413d
Marcel Zep <marcel.zep@scv.si>
2021-01-04 13:59:24 +0100

    Povezava z bazo!

---
+++ konj/konj/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Configuration;
+
+namespace konj
+{
+        
+        public class BazaConn
+        {
+            public static string connect()
+            {
+                var uriString = ConfigurationManager.AppSettings["ELEPHANTSQL_URL"] ?? "postgres://gqacvxla:FQ7xgQaI0YV-IGCJiaLDY7oUjFnoYBPx@kandula.db.elephantsql.com:5432/gqacvxla";
+                var uri = new Uri(uriString);
+                var db = uri.AbsolutePath.Trim('/');
+                var user = uri.UserInfo.Split(':')[0];
+                var passwd = uri.UserInfo.Split(':')[1];
+                var port = uri.Port > 0 ? uri.Port : 5432;
+                var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+                uri.Host, db, user, passwd, port);
+                return connStr;
+            }
+
+        }
+        
+}
+
+        
+
--- konj/konj/Form1.cs
+++ konj/konj/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+using System.Configuration;
 
 namespace konj
 {
@@ -16,5 +18,38 @@ namespace konj
         {
             InitializeComponent();
         }
+
+        string connect = BazaConn.connect();
+        public void IzpisKrajev()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+
+                NpgsqlCommand com = new NpgsqlCommand("SELECT ime_k, posta FROM kraji", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ime = reader.GetString(0);
+                    int posta = reader.GetInt32(1);
+
+                    listBox1.Items.Add(ime + "        " + posta);
+                }
+
+                con.Close();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IzpisKrajev();
+        }
     }
+    
+
 }
