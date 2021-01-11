12ee9cf421142e195c42db0fa9f03cc8346af814
Marcel Zep <marcel.zep@scv.si>
2021-01-11 14:04:16 +0100

    CRUD za zobozdravnike je končan!!!

--- konj/konj/Form3.Designer.cs
+++ konj/konj/Form3.Designer.cs
@@ -29,26 +29,172 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Ime = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Priimek = new System.Windows.Forms.TextBox();
+            this.Geslo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(307, 23);
+            this.label1.Location = new System.Drawing.Point(215, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(227, 51);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Ordinacije";
+            this.label1.Size = new System.Drawing.Size(305, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zobozdravniki";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(258, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Izberite zobozdravnika:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(292, 121);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 24);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Ime
+            // 
+            this.Ime.Location = new System.Drawing.Point(212, 207);
+            this.Ime.Name = "Ime";
+            this.Ime.Size = new System.Drawing.Size(169, 22);
+            this.Ime.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(88, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 25);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Geslo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Priimek:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(88, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 25);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Ime:";
+            // 
+            // Priimek
+            // 
+            this.Priimek.Location = new System.Drawing.Point(212, 267);
+            this.Priimek.Name = "Priimek";
+            this.Priimek.Size = new System.Drawing.Size(169, 22);
+            this.Priimek.TabIndex = 32;
+            // 
+            // Geslo
+            // 
+            this.Geslo.Location = new System.Drawing.Point(212, 327);
+            this.Geslo.Name = "Geslo";
+            this.Geslo.Size = new System.Drawing.Size(169, 22);
+            this.Geslo.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(88, 386);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 25);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Kraj:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(450, 123);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(152, 24);
+            this.comboBox2.TabIndex = 35;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(160, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 81);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Shrani spremembe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(476, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 67);
+            this.button2.TabIndex = 37;
+            this.button2.Text = "Izbriši zobozdravnika";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(212, 386);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(169, 24);
+            this.comboBox3.TabIndex = 38;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(826, 549);
+            this.ClientSize = new System.Drawing.Size(760, 590);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Geslo);
+            this.Controls.Add(this.Priimek);
+            this.Controls.Add(this.Ime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label1);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +203,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox Ime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Priimek;
+        private System.Windows.Forms.TextBox Geslo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
--- konj/konj/Form3.cs
+++ konj/konj/Form3.cs
@@ -7,14 +7,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+using System.Configuration;
 
 namespace konj
 {
     public partial class Form3 : Form
     {
+        int id_kraja;
+        string connect = BazaConn.connect();
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Update()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM izpisimen()", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader.GetString(0));
+                }
+
+                con.Close();
+
+                con.Open();
+
+                NpgsqlCommand aha = new NpgsqlCommand("SELECT * FROM izpiskrajev()", con);
+                NpgsqlDataReader pff = aha.ExecuteReader();
+                while (pff.Read())
+                {
+                    comboBox3.Items.Add(pff.GetString(0));
+                }
+
+                con.Close();
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            Ime.Enabled = false;
+            Priimek.Enabled = false;
+            Geslo.Enabled = false;
+
+            Update();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+
+                NpgsqlCommand aha = new NpgsqlCommand("SELECT * FROM izpispriimkov('" + comboBox1.SelectedItem + "')", con);
+                NpgsqlDataReader ja = aha.ExecuteReader();
+                while (ja.Read())
+                {
+                    comboBox2.Items.Add(ja.GetString(0));
+                }
+
+                con.Close();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Ime.Enabled = true;
+            Priimek.Enabled = true;
+            Geslo.Enabled = true;
+
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+
+                NpgsqlCommand aha = new NpgsqlCommand("SELECT * FROM izpiszobozdravnikov('" + comboBox1.SelectedItem + "', '" + comboBox2.SelectedItem + "')", con);
+                NpgsqlDataReader ja = aha.ExecuteReader();
+                while (ja.Read())
+                {
+                    Ime.Text = ja.GetString(0);
+                    Priimek.Text = ja.GetString(1);
+                    Geslo.Text = ja.GetString(2);
+                    comboBox3.SelectedIndex = ja.GetInt32(3) - 1;
+                }
+
+                con.Close();
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+
+                NpgsqlCommand ahda = new NpgsqlCommand("SELECT dobitkrajid('" + comboBox3.SelectedItem + "')", con);
+                NpgsqlDataReader ja = ahda.ExecuteReader();
+                while (ja.Read())
+                {
+                    id_kraja = ja.GetInt32(0);
+                }
+
+                con.Close();
+
+
+                con.Open();
+                NpgsqlCommand aha = new NpgsqlCommand("SELECT posodobizobozdravnika('" + comboBox1.SelectedItem + "', '" + Ime.Text + "', '" + comboBox2.SelectedItem + "', '" + Priimek.Text + "', '" + Geslo.Text + "', '" + id_kraja + "')", con);
+                aha.ExecuteNonQuery();
+                aha.Dispose();
+                con.Close();
+            }
+            comboBox1.Items.Clear();
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            Ime.Text = "";
+            Priimek.Text = "";
+            Geslo.Text = "";
+            Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+                NpgsqlCommand aha = new NpgsqlCommand("SELECT izbrisizobozdravnika('" + comboBox1.SelectedItem + "', '" + comboBox2.SelectedItem + "') ", con);
+                aha.ExecuteNonQuery();
+                aha.Dispose();
+                con.Close();
+            }
+
+            comboBox1.Items.Clear();
+            comboBox1.SelectedIndex = -2;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            Ime.Text = "";
+            Priimek.Text = "";
+            Geslo.Text = "";
+            Update();
         }
     }
 }
